67ec29b83f01ae4475e3ecd52c44e914bcf39bea
Bekbolsun <linex1827@gmail.com>
2022-04-16 20:44:32 +0600

    DZ_Bekbolsun

--- DZ4/greeting.js
+++ DZ4/greeting.js
@@ -1,1 +1,35 @@
-const r =[]
+// console.log(document)
+
+const blocks = document.getElementsByClassName('block');
+// console.log(blocks)
+
+const block3 = document.getElementById('block3')
+// console.log(block3.classList)
+// block3.className = "changedClass"
+// console.log(block3.classList)
+// block3.classList.add("changedClass")
+// block3.classList.remove("newClass")
+// console.log(block3.classList)
+
+// block3.onclick = () => {
+//     block3.classList.toggle("active")
+// }
+
+// const block = document.querySelector('.block')
+//
+// const blocks1 = document.querySelectorAll('.block')
+// console.log(blocks1)
+
+const button = document.getElementById('button')
+const input = document.getElementById('input')
+
+button.onclick = () => {
+    const div = document.createElement('div')
+    const deleteButton = document.createElement('button')
+    deleteButton.innerText = "delete";
+    
+    div.setAttribute("class", 'block');
+    div.innerText = input.value
+    document.body.append(div)
+    input.value = "";
+}
