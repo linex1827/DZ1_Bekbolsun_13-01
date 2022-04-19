587de24eb3d0a4178180fb93998d6028d763d43d
Bekbolsun <linex1827@gmail.com>
2022-04-19 18:08:34 +0600

    DZ4

---
+++ DZ4/main.js
@@ -0,0 +1,25 @@
+const buttonminus = document.getElementById("buttonminus")
+const buttonplus = document.getElementById("buttonplus")
+const counter = document.getElementById("counter")
+const reset = document.getElementById("reset")
+let num = 0;
+
+buttonplus.onclick = () => {
+    num++
+    counter.innerText = num.toString()
+}
+
+buttonminus.onclick = () =>{
+    num--
+    if(num < 1){
+        num = 0
+    }
+    counter.innerText = num.toString()
+}
+
+reset.onclick =  () => {
+    num = 0
+    counter.innerText = num.toString()
+}
+
+
