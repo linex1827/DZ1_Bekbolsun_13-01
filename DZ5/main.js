6ef5a030b40a3f171adef0e6cd23953107689ab4
Bekbolsun <linex1827@gmail.com>
2022-04-22 18:25:49 +0600

    DZ5_Bekbolsun

--- DZ5/main.js
+++ DZ5/main.js
@@ -2,10 +2,12 @@ const not = prompt("Выберите цвет? RED,YELLOW,GREEN")
 
 
 switch (not) {
+    
 
     case "RED":
     const RED = document.querySelector('.block');
     RED.style.backgroundColor = "red";
+    console.log("STOP")
     RED.innerHTML = 'STOOP';
     RED.style.display = "flex";
     RED.style.alignItems = "center";
@@ -16,6 +18,7 @@ switch (not) {
     case "YELLOW":
     const yellow = document.querySelector('.block2');
     yellow.style.backgroundColor = "yellow";
+    console.log("WAIT")
     yellow.innerHTML = 'WAIT';
     yellow.style.display = "flex";
     yellow.style.alignItems = "center";
@@ -26,6 +29,7 @@ switch (not) {
     case "GREEN":
     const green = document.querySelector('.block3');
     green.style.backgroundColor = "green";
+    console.log("GO")
     green.innerHTML = 'GO';
     green.style.display = "flex";
     green.style.alignItems = "center";
